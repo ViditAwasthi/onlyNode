b43d06ba60cdbba60c53b3c28d45529aede99631
Vidit Awasthi <viditawasthi2@gmail.com>
2021-03-03 22:24:08 +0530

    Initial Commit

---
+++ superheros/index.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+var supervillain = require('supervillains');
+
+var mySuper = superheroes.random();
+var mySuperVil = supervillain.random();
+
+console.log(mySuper);
+console.log(mySuperVil);
